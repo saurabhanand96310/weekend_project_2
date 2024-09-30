cce76a2cd2f4242aac7ea796cb628ec4cb299cf1
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-09-30 15:40:53 +0530

    tic tak toe completed

--- 11_days_project_1/tic-tac-toe/script.js
+++ 11_days_project_1/tic-tac-toe/script.js
@@ -7,14 +7,19 @@ let c1="";
 let c2="";
 let tem0=true;
 const subbtn=document.getElementById("submit");
-subbtn.addEventListener("click", ()=>{
+subbtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    if(p1.value === "" || p2.value === "") {
+        alert("Both fields are required");
+        return;
+    }
     c1=p1.value
     c2=p2.value
     console.log(c1,c2);
     messagebox.innerText=`${c1}, you're up`;
     tem0=false;
+    enableBoxs();
 })
-
 
 let winnerPattern=[
     [0,1,2],
@@ -42,12 +47,26 @@ box.addEventListener("click", ()=>{
    
 })
 })
+
+const disableBoxs=()=>{
+    for(let box of boxs){
+        box.disabled=true;
+    }
+}
+const enableBoxs=()=>{
+    for(let box of boxs){
+        box.disabled=false;
+        box.innerText="";
+    }
+}
+
 const showWinner=(winner, tem0)=>{
     if(tem0){
         messagebox.innerText=`${c2} congratulations you won!`
     }else{
         messagebox.innerText=`${c1} congratulations you won!`
     }
+    disableBoxs();
 
 }
 
@@ -58,8 +77,11 @@ function winnerfn(){
         let value3= boxs[pattern[2]].innerText;
         if(value1 != "" && value2!= "" && value3!=""){
             if(value1===value2 && value2===value3){
-                console.log("winner")
+                // console.log("winner")
                 showWinner(value1)
+                boxs[pattern[0]].style.backgroundColor="green";
+                boxs[pattern[1]].style.backgroundColor="green"
+                boxs[pattern[2]].style.backgroundColor="green"
             }
         }
     }
